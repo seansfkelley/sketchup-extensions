a92f6dc1e968c77c737c98a2c3593e76137717ba
Sean Kelley <seansfkelley@users.noreply.github.com>
2025-11-03 10:38:31 -0800

    Knobs

--- Shaker Cabinets/src/main.rb
+++ Shaker Cabinets/src/main.rb
@@ -8,24 +8,40 @@ module ShakerCabinets
 
     return unless inputs
 
-    type, carcass_width, carcass_height, carcass_depth, face_border_width, face_depth, face_inset_depth, pull_type = inputs
+    cabinet_type, carcass_width, carcass_height, carcass_depth, face_border_width, face_depth, face_inset_depth, pull_type = inputs
 
     model = Sketchup.active_model
-    model.start_operation("Draw Shaker #{type}")
-    cabinet = model.definitions.add("Shaker #{type}")
+    model.start_operation("Draw Shaker #{cabinet_type}")
+    cabinet = model.definitions.add("Shaker #{cabinet_type}")
 
     ShakerCabinets::draw_carcass cabinet.entities, carcass_width, carcass_height, carcass_depth
-    case inputs[0]
-    when 'Cabinet (Center Pull)'
-      ShakerCabinets::draw_shaker_face cabinet.entities, carcass_width, carcass_height, face_border_width, face_depth, face_inset_depth
-    when 'Cabinet (LH Pull)'
-      ShakerCabinets::draw_shaker_face cabinet.entities, carcass_width, carcass_height, face_border_width, face_depth, face_inset_depth
-    when 'Cabinet (RH Pull)'
-      ShakerCabinets::draw_shaker_face cabinet.entities, carcass_width, carcass_height, face_border_width, face_depth, face_inset_depth
-    when 'Drawer'
+    case cabinet_type
+    when 'Cabinet (Center Pull)', 'Cabinet (LH Pull)', 'Cabinet (RH Pull)', 'Drawer'
       ShakerCabinets::draw_shaker_face cabinet.entities, carcass_width, carcass_height, face_border_width, face_depth, face_inset_depth
     when 'Drawer (No Inset)'
       ShakerCabinets::draw_simple_face cabinet.entities, carcass_width, carcass_height, face_depth
+    end
+
+    pull_point, pull_orientation = case cabinet_type
+    when 'Cabinet (Center Pull)'
+      [Geom::Point3d.new(carcass_width / 2,                     -face_depth,                    carcass_height - face_border_width / 2), :horizontal]
+    when 'Cabinet (LH Pull)'
+      [Geom::Point3d.new(face_border_width / 2,                 -face_depth,                    carcass_height - face_border_width), :vertical]
+    when 'Cabinet (RH Pull)'
+      [Geom::Point3d.new(carcass_width - face_border_width / 2, -face_depth,                    carcass_height - face_border_width), :vertical]
+    when 'Drawer'
+      [Geom::Point3d.new(carcass_width / 2,                     -face_depth + face_inset_depth, carcass_height / 2), :horizontal]
+    when 'Drawer (No Inset)'
+      [Geom::Point3d.new(carcass_width / 2,                     -face_depth,                    carcass_height / 2), :horizontal]
+    end
+
+    case pull_type
+    when 'None'
+      # nothing
+    when 'Knob'
+      ShakerCabinets::draw_knob cabinet.entities, pull_point
+    when 'Pull'
+      ShakerCabinets::draw_pull cabinet.entities, pull_point, pull_orientation
     end
 
     model.active_entities.add_instance cabinet, ORIGIN
@@ -35,10 +51,10 @@ module ShakerCabinets
 
   def self.draw_carcass(entities, width, height, depth)
     face = entities.add_face [
-      [0, 0, 0],
+      [0,     0, 0],
       [width, 0, 0],
       [width, 0, height],
-      [0, 0, height],
+      [0,     0, height],
     ]
     face.reverse! unless face.normal.samedirection?(Y_AXIS)
     face.pushpull depth
@@ -74,6 +90,17 @@ module ShakerCabinets
     ]
     face.reverse! unless face.normal.samedirection?(Y_AXIS)
     face.pushpull face_depth
+  end
+
+  def self.draw_knob(entities, point)
+    circle = entities.add_circle point, Y_AXIS.reverse, (1.to_f / 2).inch
+    circle[0].find_faces # actually create the circle face
+    face = circle[0].faces.first
+    face.pushpull 1.inch
+  end
+
+  def self.draw_pull(entities, point, orientation)
+
   end
 end
 
